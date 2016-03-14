f432602ff259244c76494ab2c43db416ca095b42
wangkun <a6528328@163.com>
2016-03-14 17:57:49 +0800

    dfdf

--- app/src/main/java/lenannan/com/test/MainActivity.java
+++ app/src/main/java/lenannan/com/test/MainActivity.java
@@ -14,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
         String a = new String();
 
         String b = new String();
+        if ("ok".equals(a)){
+            Toast.makeText(MainActivity.this, "dfdfdf", Toast.LENGTH_SHORT).show();
+        }
+        if ("off".equals(b)){
 
+        }
     }
 }
